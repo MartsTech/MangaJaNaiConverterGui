82c8e4c5237f54420886dca98a73847b4af66f97
Martin Velkov <72291444+MartsTech@users.noreply.github.com>
2026-01-01 11:12:23 +0200

    fix attempt 2

--- MangaJaNaiConverterGui/backend/src/run_upscale.py
+++ MangaJaNaiConverterGui/backend/src/run_upscale.py
@@ -385,19 +385,19 @@ def ai_upscale_image(
             if image.dtype == np.float32 or image.dtype == np.float64:
                 image = (image * 255.0).clip(0, 255).astype(np.uint8)
 
-            needs_squeeze = (image.ndim == 2)
-
-            if needs_squeeze:
+            if image.ndim == 2:
                 image = np.expand_dims(image, axis=2)
 
-            image = np.ascontiguousarray(image)
+            # image = np.ascontiguousarray(image)
             result = model.upscale_image(image, overlap=16)
 
             if result.dtype == np.uint8:
                 result = result.astype(np.float32) / 255.0
-        
-            if needs_squeeze and result.ndim == 3:
-                result = np.squeeze(result, axis=2)
+            
+            _, _, c = get_h_w_c(result)
+
+            if c == 1 and result.ndim == 3:
+                result = np.squeeze(result, axis=-1)
 
             return result
 
